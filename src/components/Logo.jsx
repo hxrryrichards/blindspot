4f8f1bf61415282b6cf4f10133241a4ed5d6f76f
Harry Richards <harry@blindspot.agency>
2026-08-15 08:54:31 +0000

    File changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,13 @@
 const LIGHT_LOGO =
-  'https://media.base44.com/images/public/6a7f217dc1ce7582bf8d14c5/77938d6a0_BlindspotLogoTransparentLight.PNG';
+  'https://media.base44.com/images/public/6a7f217dc1ce7582bf8d14c5/9a862a711_Untitleddesign3.PNG';
 const DARK_LOGO =
-  'https://media.base44.com/images/public/6a7f217dc1ce7582bf8d14c5/0710ea0c8_BlindspotLogoTransparentDark.PNG';
+  'https://media.base44.com/images/public/6a7f217dc1ce7582bf8d14c5/9ba937894_Untitleddesign4.PNG';
 
 export default function Logo({ className = '' }) {
   return (
     <span className={`inline-flex items-center ${className}`}>
-      <img src={DARK_LOGO} alt="Blindspot" className="h-full w-auto dark:hidden" />
-      <img src={LIGHT_LOGO} alt="Blindspot" className="hidden h-full w-auto dark:block" />
+      <img src={LIGHT_LOGO} alt="Blindspot" className="h-full w-auto dark:hidden" />
+      <img src={DARK_LOGO} alt="Blindspot" className="hidden h-full w-auto dark:block" />
     </span>
   );
 }
